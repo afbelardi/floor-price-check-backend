f2ff4b058113eb869f484efb1cf8bff6b573c23a
afbelardi <afbelardi@gmail.com>
2023-03-22 13:03:07 -0400

    initial server setup

---
+++ server.js
@@ -0,0 +1,37 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 8000;
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+app.use(cors());
+
+const MONGODB_URI = process.env.MONGODB_URI;
+const db = mongoose.connection;
+
+const urlencodedParser = bodyParser.urlencoded({ extended: false });
+app.use(bodyParser.json(), urlencodedParser);
+
+mongoose.connect(MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+
+db.on('open', () => {
+    console.log('MongoDB is connected')
+});
+
+
+
+app.use(express.json());
+if (process.env.NODE_ENV !== 'development'){
+  app.use(express.static('public'))
+}
+app.use(/\.[0-9a-z]+$/i, express.static('public'));
+
+app.listen(PORT, () => {
+    console.log(`API Listening on port ${PORT}`);
+});
