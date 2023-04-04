9ca2db95af7513a56ef47b53153485fab096d9f0
afbelardi <afbelardi@gmail.com>
2023-04-04 16:23:35 -0400

    yet another cors change

--- puppeteer.js
+++ puppeteer.js
@@ -52,6 +52,8 @@ searchRouter.post('/search', async (req, res) => {
        nftImage: nftImage
     }
     res.set('Access-Control-Allow-Origin', '*');
+    res.set('Access-Control-Allow-Headers', 'Content-Type');
+    
     res.send(result)
     await browser.close();      
  
