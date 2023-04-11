28853d66c8acee0db8f5f746ddb25d3f8450c265
afbelardi <afbelardi@gmail.com>
2023-04-11 11:07:46 -0400

    added dockerfile

--- puppeteer.js
+++ puppeteer.js
@@ -6,16 +6,17 @@ const searchRouter = express.Router();
 
 
 searchRouter.post('/search', async (req, res) => {
-      try {
-        const searchTerm = req.body.searchTerm;
-
-    const searchElement = 'div.NavSearch--mobile-search-input input[role="searchbox"]';
-    
-        const browser = await puppeteer.launch({
+    const browser = await puppeteer.launch({
         args: [
             '--no-sandbox', 
         ],
     });
+    try {
+        const searchTerm = req.body.searchTerm;
+
+    const searchElement = 'div.NavSearch--mobile-search-input input[role="searchbox"]';
+    
+
     const page = await browser.newPage();
     await page.setViewport({width: 1920, height: 1080});
     // await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.181 Safari/537.36')
@@ -28,42 +29,42 @@ searchRouter.post('/search', async (req, res) => {
     await page.type(searchElement, searchTerm);
 
 
-    // // await new Promise(r => setTimeout(r, 4000));
+    // await new Promise(r => setTimeout(r, 4000));
 
-    // const collectionElement = 'a[class*="sc-1f719d57-0 fKAlPV sc-29427738-0 sc-630fc9ab-0 sc-396bc2e1-0 hyHTWC jSPhMX gslsVL sc-581b878c-0 jPMaKh"]'
-    // await page.waitForSelector(collectionElement);
-    // const link = await page.$(collectionElement);
+    const collectionElement = 'a[class*="sc-1f719d57-0 fKAlPV sc-29427738-0 sc-630fc9ab-0 sc-396bc2e1-0 hyHTWC jSPhMX gslsVL sc-581b878c-0 jPMaKh"]'
+    await page.waitForSelector(collectionElement);
+    const link = await page.$(collectionElement);
   
-    // await link.click();
+    await link.click();
 
-    // await new Promise(r => setTimeout(r, 1000));
-    // await page.waitForSelector('.sc-29427738-0.sc-d58c749b-1.ILliQ.jsHA-dC')
+    await new Promise(r => setTimeout(r, 1000));
+    await page.waitForSelector('.sc-29427738-0.sc-d58c749b-1.ILliQ.jsHA-dC')
 
-    // const h1Element = await page.$('h1');
-    // const h1Text = await page.evaluate(h1 => h1.textContent, h1Element);
+    const h1Element = await page.$('h1');
+    const h1Text = await page.evaluate(h1 => h1.textContent, h1Element);
    
     
-    // const firstChild = await page.$$('.sc-29427738-0.sc-d58c749b-1.ILliQ.jsHA-dC');
-    // const firstChildTest = firstChild[1]
-    // const firstChildText = await (await firstChildTest.getProperty('textContent')).jsonValue();
-    // const [value, currency] = firstChildText.split(/([a-zA-Z]+)/);
+    const firstChild = await page.$$('.sc-29427738-0.sc-d58c749b-1.ILliQ.jsHA-dC');
+    const firstChildTest = firstChild[1]
+    const firstChildText = await (await firstChildTest.getProperty('textContent')).jsonValue();
+    const [value, currency] = firstChildText.split(/([a-zA-Z]+)/);
 
 
-    // const nftImage = await page.$eval('img.sc-6d5b054-0.jhsaBJ[src]', img => img.src);
+    const nftImage = await page.$eval('img.sc-6d5b054-0.jhsaBJ[src]', img => img.src);
 
 
-    // const result = {
-    //    floorPrice: `${value} ${currency}`,
-    //    collectionName: h1Text,
-    //    nftImage: nftImage
-    // }
+    const result = {
+       floorPrice: `${value} ${currency}`,
+       collectionName: h1Text,
+       nftImage: nftImage
+    }
 
-    // res.send(result)
-    await browser.close();  
-    res.send('worked')    
+    res.send(result)
     
     } catch (error) {
         console.error(error)
+    } finally {
+        await browser.close();  
     }
 })
 
