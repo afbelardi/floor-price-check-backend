662239a95ef5c46ef8b78ef60aec3ce5b5e8c26a
afbelardi <afbelardi@gmail.com>
2023-04-12 14:43:12 -0400

    puppeteer change

--- puppeteer.js
+++ puppeteer.js
@@ -28,10 +28,9 @@ searchRouter.post('/search', async (req, res) => {
     const page = await browser.newPage();
     await page.setViewport({width: 1920, height: 1080});
     // await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.181 Safari/537.36')
-    await page.goto('https://www.opensea.io/', { timeout: 800000 });
+    await page.goto('https://www.opensea.io/', { timeout: 0 });
     await page.screenshot();
     console.log('made it this far')
-    await page.waitForNavigation();
     await page.waitForSelector(searchElement);
     await page.focus(searchElement);
     await page.type(searchElement, searchTerm);
